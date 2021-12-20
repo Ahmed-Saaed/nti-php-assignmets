b1b0dd72856d5af93490b86608ecf99e4c83e32a
Ahmed-Saaed <86948480+Ahmed-Saaed@users.noreply.github.com>
2021-12-20 23:36:51 +0200

    update edit file

--- edit.php
+++ edit.php
@@ -3,57 +3,58 @@
 require '../helpers/dbConnection.php';
 require '../helpers/functions.php';
 
-# Fetch Roles .....
-$sql = 'select * from category';
-$categories = mysqli_query($con, $sql);
-
+# Get Data related to id ......
 $id = $_GET['id'];
 
-$sqlb = "select * from blog where id = '$id'";
-$opb = mysqli_query($con , $sqlb);
-$blogData = mysqli_fetch_assoc($opb);
+$sql = "select * from users where id = $id";
+$op = mysqli_query($con, $sql);
 
+if (mysqli_num_rows($op) == 1) {
+    $data = mysqli_fetch_assoc($op);
+} else {
+    $_SESSION['Message'] = ['Message' => 'Access Denied'];
+    header('Location: ' . url('Users/index.php'));
+}
+
+# Fetch Role Data .....
+$sql = 'select * from roles';
+$roleData = mysqli_query($con, $sql);
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // CODE ......
-    $title   = Clean($_POST['title']);
-    $content = Clean($_POST['content']);
-    $date    = Clean($_POST['date']);
-    $cat_id  = $_POST['cat_id'];
+    $name = Clean($_POST['name']);
+    $email = Clean($_POST['email']);
+    //$password = Clean($_POST['password']);
+    $role_id = $_POST['role_id'];
+    $phone = Clean($_POST['phone']);
 
     # Validation ......
     $errors = [];
-
     # Validate Name
-    if (!validate($title, 1)) {
-        $errors['Title'] = 'Field Required';
-    } elseif (!validate($title, 7)) {
-        $errors['Title'] = 'Invalid String';
+    if (!validate($name, 1)) {
+        $errors['Name'] = 'Field Required';
+    } elseif (!validate($name, 7)) {
+        $errors['Name'] = 'Invalid String';
     }
 
     # Validate Email
-    if (!validate($content, 1)) {
-        $errors['Content'] = 'Field Required';
-    }  elseif (!validate($content, 3, 10)) {
-        $errors['Content'] = 'Length Must >= 10 chs';
+    if (!validate($email, 1)) {
+        $errors['Email'] = 'Field Required';
+    } elseif (!validate($email, 2)) {
+        $errors['Email'] = 'Invalid Email Format';
     }
 
-
-
-    # Validate cat_id
-    if (!validate($cat_id, 4)) {
-        $errors['Category'] = 'Invalid Category';
+    # Validate role_id
+    if (!validate($role_id, 4)) {
+        $errors['Role'] = 'Invalid Role';
     }
 
-    
-            // validate date : $date_arr  = explode('/', $date_str):if (!checkdate($date_arr[0], $date_arr[1], $date_arr[2]))
-    # Validate date
-    if (!validate($date, 1)) {
-        $errors['date'] = 'Field Required';
-    }elseif(!validate($date,8)){
-        $errors['date'] = 'enter valied date';
+    # Validate phone
+    if (!validate($phone, 1)) {
+        $errors['phone'] = 'Field Required';
+    } elseif (!validate($phone, 6)) {
+        $errors['phone'] = 'Invalid Phone Number';
     }
-
 
     # Validate image
     if (validate($_FILES['image']['name'], 1)) {
@@ -65,7 +66,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $exArray = explode('.', $imageName);
         $extension = end($exArray);
 
-        $FinalName = rand().time().'.'.$extension;
+        $FinalName = rand() . time() . '.' . $extension;
 
         $allowedExtension = ['png', 'jpg'];
 
@@ -80,7 +81,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         // db ..........
 
         // old Image
-        $OldImage = $blogData['image'];
+        $OldImage = $data['image'];
 
         if (validate($_FILES['image']['name'], 1)) {
             $desPath = './uploads/' . $FinalName;
@@ -92,25 +93,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $FinalName = $OldImage;
         }
 
-        $sql = "update blog set title = '$title' , content = '$content', image = '$FinalName' , date = '$date' , cat_id = '$cat_id'  where id = '$id'";
+        $sql = "update users set name = '$name' , email = '$email',image = '$FinalName' , role_id = $role_id , phone = '$phone' where id = $id";
         $op = mysqli_query($con, $sql);
-
-  
 
         if ($op) {
             $_SESSION['Message'] = ['message' => 'Raw Updated'];
-            
 
-            header('Location: ' . url('Articles/index.php'));
+            header('Location: ' . url('Users/index.php'));
             exit();
         } else {
-          echo '<br>';
-          echo '<br>';
-            echo mysqli_error($con);
             $_SESSION['Message'] = ['message' => 'Error Try Again'];
         }
     }
 }
+
 require '../layouts/header.php';
 require '../layouts/nav.php';
 require '../layouts/sidNav.php';
@@ -118,109 +114,105 @@ require '../layouts/sidNav.php';
 
 
 <div id="layoutSidenav_content">
-  <main>
-    <div class="container-fluid">
+    <main>
+        <div class="container-fluid">
 
 
 
 
-      <h1 class="mt-4">Dashboard</h1>
-      <ol class="breadcrumb mb-4">
+            <h1 class="mt-4">Dashboard</h1>
+            <ol class="breadcrumb mb-4">
 
-        <?php 
+                <?php 
                             
                               if(isset($_SESSION['Message'])){
                                 foreach($_SESSION['Message'] as $key => $val){
-                                echo '* '.$key.' : '.$val.'<br>';
+                                echo '* '.$key.' : '.$val;
                                 }
                                 unset($_SESSION['Message']); 
                             }else{
                             
                             ?>
 
-        <li class="breadcrumb-item active">Dashboard/Add User</li>
+                <li class="breadcrumb-item active">Dashboard/Edit Role</li>
 
-        <?php } ?>
-      </ol>
-
-
+                <?php } ?>
+            </ol>
 
 
 
-
-      <div class="card-body">
-
-
-        <div class="container">
+            <div class="card-body">
 
 
-
-          <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post"
-            enctype="multipart/form-data">
+                <div class="container">
 
 
 
-            <div class="form-group">
-              <label for="exampleInputName">Title</label>
-              <input type="text" class="form-control" id="exampleInputName" name="title" aria-describedby=""
-                placeholder="Enter Title" value="<?php echo $blogData['title']; ?>">
-            </div>
-
-
-            <div class="form-group">
-              <label for="exampleInputEmail">Content</label>
-              <textarea type="text" class="form-control" name="content"
-                placeholder="Enter your content"><?php echo $blogData['content'];?></textarea>
-            </div>
-
-            <div class="form-group">
-              <label for="exampleInputPassword">date</label>
-              <input type="date" class="form-control" id="exampleInputPassword1" name="date" placeholder="date"
-                value="<?php echo date("Y/m/d",$blogData['date']); ?>">
-            </div>
+                    <form action="edit.php?id=<?php echo $data['id']; ?>" method="post" enctype="multipart/form-data">
 
 
 
 
+                        <div class="form-group">
+                            <label for="exampleInputName">Name</label>
+                            <input type="text" class="form-control" id="exampleInputName" name="name"
+                                value="<?php echo $data['name']; ?>" aria-describedby="" placeholder="Enter Name">
+                        </div>
 
-            <div class="form-group">
-              <label for="exampleInputPassword">Category</label>
 
-              <select class="form-control" name="cat_id">
-                <?php
-                                while($data = mysqli_fetch_assoc($categories)){
+                        <div class="form-group">
+                            <label for="exampleInputEmail">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" name="email"
+                                value="<?php echo $data['email']; ?>" aria-describedby="emailHelp" placeholder="Enter email">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="exampleInputPassword"> Phone</label>
+                            <input type="phone" class="form-control" value="<?php echo $data['phone']; ?>"
+                                id="exampleInputPassword1" name="phone" placeholder="phone">
+                        </div>
+
+
+                        <div class="form-group">
+                            <label for="exampleInputPassword">Roles</label>
+
+                            <select class="form-control" name="role_id">
+                                <?php 
+                               while($roledetails = mysqli_fetch_assoc($roleData)){
                                 ?>
-                <option value="<?php echo $data['id']; ?>"><?php echo $data['title']; ?></option>
-                <?php } ?> ?>
-              </select>
+                                <option value="<?php echo $roledetails['id']; ?>" <?php if ($data['role_id'] == $roledetails['id']) {
+    echo 'selected';
+} ?>><?php echo $roledetails['title']; ?></option>
+                                <?php } ?>
+                            </select>
+                        </div>
+
+
+                        <div class="form-group">
+                            <label for="exampleInputPassword">Image</label><br>
+                            <input type="file" name="image">
+                        </div>
+
+                        <img src="./uploads/<?php echo $data['image']; ?>" height="80" width="80"><br>
+
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+
+
+
+
+
+
+
+                </div>
             </div>
-
-
-
-            <div class="form-group">
-              <label for="exampleInputPassword">Image</label><br>
-              <input type="file" name="image">
-            </div>
-
-
-            <button type="submit" class="btn btn-primary">Submit</button>
-          </form>
-
-
-
-
-
 
 
         </div>
-      </div>
+    </main>
 
 
-    </div>
-  </main>
-
-
-  <?php
+    <?php
     
     require '../layouts/footer.php';
     ?>
